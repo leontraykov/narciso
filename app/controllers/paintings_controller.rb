7cde4a9ac6697f6a01e13c6def66d5eade3219d5
leont <leon.traykov@gmail.com>
2023-09-29 15:31:23 -0400

    version 1

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,4 +1,5 @@
 class PaintingsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy] 
   before_action :set_painting, only: %i[show edit update destroy]
 
   def index
