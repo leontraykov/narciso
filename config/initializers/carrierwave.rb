693ff9885c6226b87b113101bb2d245ef260ec66
leont <leon.traykov@gmail.com>
2023-09-28 22:04:11 -0400

    predeploy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
-    provider:              'AWS',
-    use_iam_profile:       true,
-    region:                'us-east-1' # Пример региона, укажите ваш регион
+    provider: 'AWS',
+    region: 'us-east-1',  # Замените на регион вашего ведра
+    use_iam_profile: true
   }
   config.fog_directory  = 'narciso-images'
 end
